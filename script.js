323fffd6b877f1df6b3d6e9ed88f8e932caaecba
ismailPervez <freelanceismail001@gmail.com>
2021-07-18 17:47:45 +0300

    this is the 5th commit - i have added logic to the web app and its now fully functioning

---
+++ script.js
@@ -0,0 +1,51 @@
+const DAYS = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+
+const NAMES = {
+    "male": 
+        {
+            "sunday": "Kwasi",
+            "monday": "Kwadwo",
+            "tuesday": "Kwabena",
+            "wednesday": "Yaw",
+            "thursday": "Kofi",
+            "friday": "Kwabena",
+            "saturday": "Kwame"
+        },
+    "female": 
+        {
+            "sunday": "Akosua",
+            "monday": "Adwoa",
+            "tuesday": "Abenaa",
+            "wednesday": "Akua",
+            "thursday": "Yaa",
+            "friday": "Afua",
+            "saturday": "Ama"
+        }
+}
+
+let birthday = document.getElementById('birthday');
+let gender = document.getElementById('gender');
+let submitBtn = document.getElementById('submit-btn');
+let results = document.getElementById("result-name");
+
+submitBtn.onclick = (event) => {
+    event.preventDefault();
+    let genderValue = gender.options[gender.selectedIndex].value;
+    let birthdayValue = birthday.value;
+
+    // get day of the week from the birthday
+    let officialBirthDay = new Date(birthdayValue)
+    let day = DAYS[officialBirthDay.getDay()];
+
+    if (genderValue === "male") {
+        let akanName = NAMES.male[day];
+        // console.log(akanName);
+        results.innerHTML = akanName;
+    }
+
+    else {
+        let akanName = NAMES.female[day];
+        // console.log(akanName);
+        results.innerHTML = akanName;
+    }
+}
